52efaaed3816151e1f28726992ccded79c9490ad
oibasqada <tumarbekovmax@gmail.com>
2023-03-22 12:12:45 +0600

    Continued

--- script.js
+++ script.js
@@ -1,15 +1,47 @@
-const button = document.querySelector('button');
-const input = document.querySelector('#input');
-    let num = 0;
-button.addEventListener('click', () => {
+const buttonPlus = document.querySelector('#btnPlus');
+const buttonMinus = document.querySelector('#btnMinus');
+const result = document.querySelector('.result')
+// const input = document.querySelector('#input');
 
+let num = 0;
+result.innerText = num;
+buttonPlus.addEventListener('click', () => {
     num += 1;
-    console.log(num);
+    result.innerText = num;
+
+    if (num > 10) {
+        num -= 1
+        result.innerText = num;
+        // buttonMinus.disabled = true;
+    } else if (num > 10) {
+        return buttonMinus.disabled = true;
+    }
 });
 
-input.addEventListener('input', (event) => {
-    console.log(event.target.value)
+buttonMinus.addEventListener('click', () => {
+    num -= 1;
+    result.innerText = num;
+
+    if (num < 0) {
+        num += 1
+        result.innerText = num;
+        // buttonMinus.disabled = true;
+    } else if (num < 0) {
+        return buttonMinus.disabled = true;
+    }
 });
+
+    // if (num > 10) {
+    //     num -= 1
+    //     result.innerText = num;
+    //     // buttonMinus.disabled = true;
+    // } else if ( num < 0) {
+    //     num += 1
+    //     result.innerText = num;
+    // }
+// input.addEventListener('input', (event) => {
+//     console.log(event.target.value)
+// });
 
 
 
