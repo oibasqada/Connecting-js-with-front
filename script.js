6271a1a97c01833880eb243b163b4de526b0b86b
oibasqada <tumarbekovmax@gmail.com>
2023-03-22 20:26:30 +0600

    Added multiply and devide buttons on result

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const buttonPlus = document.querySelector('#btnPlus');
 const buttonMinus = document.querySelector('#btnMinus');
+const buttonDegree = document.querySelector('#btnDgree');
+const buttonDevide = document.querySelector('#btnDvide');
 const result = document.querySelector('.result')
 // const input = document.querySelector('#input');
 
 let num = 0;
 result.innerText = num;
 buttonPlus.addEventListener('click', () => {
-    if (num < 10) {
+    if (num < 1000) {
         num += 1;
         result.innerText = num;
     } else {
@@ -23,6 +25,35 @@ buttonMinus.addEventListener('click', () => {
         alert('Hey! Stob it')
     }
 });
+
+//делать каунтер, добавить умножение и деление на result
+
+buttonDegree.addEventListener('click', () => {
+    result.innerText = num * num;
+})
+
+buttonDevide.addEventListener('click', () => {
+    result.innerText = num / 2;
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // input.addEventListener('input', (event) => {
 //     console.log(event.target.value)
